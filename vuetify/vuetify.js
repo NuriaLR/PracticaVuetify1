bd79ab388f49c9948cf8e612a94d9f25389b100f
nuria <nuria.l.nlr@gmail.com>
2024-02-28 19:00:35 +0100

    Añadido para poder fusionar rama

--- vuetify/vuetify.js
+++ vuetify/vuetify.js
@@ -1,4 +1,8 @@
+// Importa la función createVuetify desde el archivo 'framework.mjs' en el paquete 'vuetify/lib'.
 import { createVuetify } from 'vuetify/lib/framework.mjs'
+
+// Importa los estilos predefinidos de Vuetify.
 import 'vuetify/styles'
 
+// Exporta una instancia de Vuetify configurada.
 export default createVuetify()
